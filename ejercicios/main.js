5359da9101299a3a15db5094d0ec6de2f499c5b1
emilianofunez <emilianofunezasolino@gmail.com>
2026-09-15 16:37:26 -0300

    Entrega modulo 1 JavaScript

---
+++ ejercicios/main.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Escriba su nombre");
+let edad = Number(prompt("Escriba su edad"));
+
+let num1 = Number(prompt("Escriba su primer numero"));
+let num2 = Number(prompt("Escriba su primer numero"));
+
+let resultado = num1 + num2;
+
+console.log("Tu nombre es " + nombre,"Tenes " + edad + " años de edad");
+console.log("El resultado de la suma es" + resultado);
+ 
+alert("Hola " + nombre + " tu resultado es " + resultado);
