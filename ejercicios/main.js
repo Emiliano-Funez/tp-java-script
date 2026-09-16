150f5da743274d774bcee7bd353a948bc83d3492
emilianofunez <emilianofunezasolino@gmail.com>
2026-09-15 21:06:35 -0300

    listo

--- ejercicios/main.js
+++ ejercicios/main.js
@@ -1,12 +1,19 @@
 let nombre = prompt("Escriba su nombre");
-let edad = Number(prompt("Escriba su edad"));
 
-let num1 = Number(prompt("Escriba su primer numero"));
-let num2 = Number(prompt("Escriba su primer numero"));
+let apellido = prompt("Escriba su apellido");
+
+let edad = parseInt(prompt("Escriba su edad"));
+
+const año = Number(prompt("Que año es hoy"));
+
+let num1 = parseFloat(prompt("Escriba su primer numero"));
+
+let num2 = parseFloat(prompt("Escriba su segundo numero"));
 
 let resultado = num1 + num2;
 
-console.log("Tu nombre es " + nombre,"Tenes " + edad + " años de edad");
-console.log("El resultado de la suma es" + resultado);
- 
-alert("Hola " + nombre + " tu resultado es " + resultado);
+console.log("Tu nombre es " + nombre + " " + apellido + " tenes " + edad + " años de edad");
+
+console.log("El resultado de la suma es " + resultado);
+
+alert("Hola " + nombre + " " + apellido + " tu resultado es " + resultado + " El año es: " + año);
